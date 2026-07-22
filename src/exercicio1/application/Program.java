c0ad5bd1d282e8e39ea4925db224c5ffb5adbf0e
Davi Cunha <davicunha910@gmail.com>
2026-07-22 08:40:19 -0300

    firstex

---
+++ src/exercicio1/application/Program.java
@@ -0,0 +1,25 @@
+package exercicio1.application;
+
+import exercicio1.entities.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle retangulo = new Rectangle();
+
+        System.out.println("Digite os valores de largura e altura: ");
+        retangulo.width = sc.nextDouble();
+        retangulo.height = sc.nextDouble();
+
+        System.out.println("Área: " + retangulo.area());
+        System.out.println("Perímetro: " + retangulo.perimeter());
+        System.out.println("Diagonal: " + retangulo.diagonal());
+
+        sc.close();
+    }
+}
